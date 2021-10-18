d34bae675c2eaf83db47bc1cf05b22b43e3a30ac
Marine <levraguemarine@gmail.com>
2021-10-18 14:45:09 +0200

    Form : en cours

--- Front-End/js/form.js
+++ Front-End/js/form.js
@@ -1,5 +1,5 @@
                                     // FORMULAIRE //
-
+/*
 // Récupération des input HTML depuis le DOM
 let firstName = document.getElementById("firstName");
 let lastName = document.getElementById("lastName");
@@ -166,3 +166,82 @@ function verifyInputBeforeCheck() {
 }
 
 verifyInputBeforeCheck();
+*/
+
+var regexText =  new RegExp(/^[\w\s'\-àáâãäæçèéêëìíîïñòóôõöùúûüýÿœÀÁÂÃÄÆÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÜŒ]+$/g); // RegExp(/^[\w\s-']+$/g)  àáâãäæçèéêëìíîïñòóôõöùúûüýÿœÀÁÂÃÄÆÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÜŒ
+var regexTextOnly = new RegExp(/^[a-zA-Z\s'\-àáâãäæçèéêëìíîïñòóôõöùúûüýÿœÀÁÂÃÄÆÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÜŒ]+$/g);
+var regexEmail = new RegExp(/^[\w\-\.]+@[a-zA-Z0-9\-]+\.[a-zA-Z]{2,6}$/g); // /^.+@.+\..+$/g
+var regexZip = new RegExp(/^\d{5}$/g);
+
+// Création d'un objet qui récupère les champs du formulaire
+let contact = {
+    firstName: null,
+    lastName: null,
+    email: null,
+    address: null,
+    city: null,
+    zip: null,
+}
+
+// Création de constantes pour récupérer les inputs
+const idFirstName = "firstName";
+const idLastName = "lastName";
+const idEmail = "email";
+const idAddress = "address";
+const idCity = "city";
+const idZip = "zip";
+
+// Récupération du message sous l'input
+let small = document.getElementById("small");
+
+// Fonction de vérification des inputs
+function checkInputs(idInput, regex) {
+    let input = document.getElementById(idInput);
+    let valueInput = input.value;
+    if(valueInput.match(regex)) {
+        contact[idInput] = valueInput;
+        small.innerHTML = "Ce champ est valide"
+        return true;
+    } else {
+        small.innerHTML = "Ce champ est incomplet ou invalide"
+        return false;
+    }
+}
+
+// Fonction d'écoute des inputs
+function listenInputs() {
+    document.getElementById(idFirstName).addEventListener("blur", () => {
+        checkInputs(idFirstName, regexTextOnly);
+    })
+    document.getElementById(idLastName).addEventListener("blur", () => {
+        checkInputs(idLastName, regexTextOnly);
+    })
+    document.getElementById(idEmail).addEventListener("blur", () => {
+        checkInputs(idEmail, regexEmail);
+    })
+    document.getElementById(idAddress).addEventListener("blur", () => {
+        checkInputs(idAddress, regexText);
+    })
+    document.getElementById(idCity).addEventListener("blur", () => {
+        checkInputs(idCity, regexTextOnly);
+    })
+    document.getElementById(idZip).addEventListener("blur", () => {
+        checkInputs(idZip, regexZip);
+    })
+
+    document.getElementById("btnValidOrder");addEventListener("click", (e) => {
+        e.preventDefault();
+        if(checkInputs(idFirstName, regexTextOnly) &&
+        checkInputs(idLastName, regexTextOnly) &&
+        checkInputs(idEmail, regexEmail) &&
+        checkInputs(idAddress, regexText) &&
+        checkInputs(idCity, regexTextOnly) &&
+        checkInputs(idZip, regexZip)) {
+            return true;
+        }else {
+            return false;
+        }
+    })
+}
+
+listenInputs();
