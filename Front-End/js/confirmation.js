3a85b3071b6e9f983e6ab46ffd752c5b4a1c0795
Marine <levraguemarine@gmail.com>
2021-10-18 19:24:23 +0200

    Form: ok Validation: ok Responsive: ok

---
+++ Front-End/js/confirmation.js
@@ -0,0 +1,25 @@
+// Fonction qui formate le prix
+function getFormatedPrice(price) {
+    let newPrice = price / 100;
+    return Intl.NumberFormat("fr-FR", {style: "currency", currency: "EUR"}).format(newPrice)
+};
+
+// Fonction qui se déclenche après le chargement de la page
+window.addEventListener("load", function() {
+    let orderInCart = JSON.parse(localStorage.order); // Retourne en objet JSON la commande complète
+    console.log(orderInCart);
+    let orderIdCart = orderInCart.orderId; // Retourne les ID des produits
+    console.log(orderIdCart);
+    let productsAsOrder = orderInCart.products; // Retourne un tableau contenant en objets les détails de chaque produit
+    console.log(productsAsOrder);
+    let totalCart = 0;
+
+    for(i in productsAsOrder){
+        totalCart += productsAsOrder[i].price;
+        console.log(totalCart);
+    }
+    document.getElementById("idOrder").innerHTML += `${orderIdCart}`;
+    document.getElementById("totalPriceOrder").innerHTML += `${getFormatedPrice(totalCart)}`;
+    localStorage.clear();
+    products = [];
+})
--- Front-End/js/form.js
+++ Front-End/js/form.js
@@ -1,177 +1,9 @@
                                     // FORMULAIRE //
-/*
-// Récupération des input HTML depuis le DOM
-let firstName = document.getElementById("firstName");
-let lastName = document.getElementById("lastName");
-let email = document.getElementById("email");
-let address = document.getElementById("address");
-let city = document.getElementById("city");
-let zip = document.getElementById("zip");
-const btnValidOrder = document.getElementById("btnValidOrder");
 
-// Récupération du formulaire
-let form = document.getElementById("form");
-console.log(form);
-
-// Déclaration des Regex
-let regexText = (/^[a-zA-Z\w\s'\-àáâãäæçèéêëìíîïñòóôõöùúûüýÿœÀÁÂÃÄÆÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÜŒ]+$/g);
-let regexOnlyText = (/^[a-zA-Z\s'\-àáâãäæçèéêëìíîïñòóôõöùúûüýÿœÀÁÂÃÄÆÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÜŒ]+$/g);
-let regexEmail = (/^[\w\-\.]+@[a-zA-Z0-9\-]+\.[a-zA-Z]{2,6}$/g);
-let regexZip = (/^\d{5}$/g);
-
-// Ecouteur de changement sur le champ prénom
-form.firstName.addEventListener("change", function() {
-    validInput(this);
-})
-
-// Vérifie que le champ prénom respecte le format attendu
-const validInput = function(firstName) {
-    let textOnly = regexOnlyText;
-    let testTextOnly = textOnly.test(firstName.value);
-    let small = firstName.nextElementSibling;
-
-    if(testTextOnly) {
-        small.innerHTML = "Le champ est correct";
-        return true;
-    }else {
-        small.innerHTML = "Le champ est vide ou incorrect";
-        return false;
-    }
-};
-
-// Ecouteur de changement sur le champ nom
-form.lastName.addEventListener("change", function() {
-    validInput2(this);
-})
-
-// Vérifie que le champ nom respecte le format attendu
-const validInput2 = function(lastName) {
-    let textOnly = regexOnlyText;
-    let testTextOnly = textOnly.test(lastName.value);
-    let small = lastName.nextElementSibling;
-
-    if(testTextOnly) {
-        small.innerHTML = "Le champ est correct";
-        return true;
-    }else {
-        small.innerHTML = "Le champ est vide ou incorrect";
-        return false;
-    }
-};
-
-// Ecouteur de changement sur le champ email
-form.email.addEventListener("change", function() {
-    validInput3(this);
-})
-
-// Vérifie que le champ email respecte le format attendu
-const validInput3 = function(email) {
-    let inputEmail = regexEmail;
-    let inputEmailOnly = inputEmail.test(email.value);
-    let small = email.nextElementSibling;
-
-    if(inputEmailOnly) {
-        small.innerHTML = "Le champ est correct";
-        return true;
-    }else {
-        small.innerHTML = "Le champ est vide ou incorrect";
-        return false;
-    }
-};
-
-// Ecouteur de changement sur le champ adresse
-form.address.addEventListener("change", function() {
-    validInput4(this);
-})
-
-// Vérifie que le champ adresse respecte le format attendu
-const validInput4 = function(address) {
-    let inputAddress = regexText;
-    let inputAddressOnly = inputAddress.test(address.value);
-    let small = address.nextElementSibling;
-
-    if(inputAddressOnly) {
-        small.innerHTML = "Le champ est correct";
-        return true;
-    }else {
-        small.innerHTML = "Le champ est vide ou incorrect";
-        return false;
-    }
-};
-
-// Ecouteur de changement sur le champ ville
-form.city.addEventListener("change", function() {
-    validInput5(this);
-})
-
-// Vérifie que le champ ville respecte le format attendu
-const validInput5 = function(city) {
-    let inputCity = regexOnlyText;
-    let inputCityOnly = inputCity.test(city.value);
-    let small = city.nextElementSibling;
-
-    if(inputCityOnly) {
-        small.innerHTML = "Le champ est correct";
-        return true;
-    }else {
-        small.innerHTML = "Le champ est vide ou incorrect";
-        return false;
-    }
-};
-
-// Ecouteur de changement sur le champ zip
-form.zip.addEventListener("change", function() {
-    validInput6(this);
-})
-
-// Vérifie que le champ zip respecte le format attendu
-const validInput6 = function(zip) {
-    let inputZip = regexZip;
-    let inputZipOnly = inputZip.test(zip.value);
-    let small = zip.nextElementSibling;
-
-    if(inputZipOnly) {
-        small.innerHTML = "Le champ est correct";
-        return true;
-    }else {
-        small.innerHTML = "Le champ est vide ou incorrect";
-        return false;
-    }
-};
-
-// Fonction d'envoi du formulaire
-function sendForm() {
-    fetch("http://localhost:3000/api/teddies/" + "order", {
-        method: "POST",
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-    })
-}
-
-// Fonction de vérification que tous les inputs sont valides
-function verifyInputBeforeCheck() {
-    form.addEventListener("submit", function(e) {
-        e.preventDefault();
-        if(validInput(form.firstName) &&
-        validInput2(form.lastName) &&
-        validInput3(form.email) &&
-        validInput4(form.address) &&
-        validInput5(form.city) &&
-        validInput6 (form.zip)) {
-            sendForm();
-        }
-    })
-}
-
-verifyInputBeforeCheck();
-*/
-
-var regexText =  new RegExp(/^[\w\s'\-àáâãäæçèéêëìíîïñòóôõöùúûüýÿœÀÁÂÃÄÆÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÜŒ]+$/g); // RegExp(/^[\w\s-']+$/g)  àáâãäæçèéêëìíîïñòóôõöùúûüýÿœÀÁÂÃÄÆÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÜŒ
-var regexTextOnly = new RegExp(/^[a-zA-Z\s'\-àáâãäæçèéêëìíîïñòóôõöùúûüýÿœÀÁÂÃÄÆÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÜŒ]+$/g);
-var regexEmail = new RegExp(/^[\w\-\.]+@[a-zA-Z0-9\-]+\.[a-zA-Z]{2,6}$/g); // /^.+@.+\..+$/g
-var regexZip = new RegExp(/^\d{5}$/g);
+let regexText =  new RegExp(/^[\w\s'\-àáâãäæçèéêëìíîïñòóôõöùúûüýÿœÀÁÂÃÄÆÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÜŒ]+$/g); // RegExp(/^[\w\s-']+$/g)  àáâãäæçèéêëìíîïñòóôõöùúûüýÿœÀÁÂÃÄÆÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÜŒ
+let regexTextOnly = new RegExp(/^[a-zA-Z\s'\-àáâãäæçèéêëìíîïñòóôõöùúûüýÿœÀÁÂÃÄÆÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÜŒ]+$/g);
+let regexEmail = new RegExp(/^[\w\-\.]+@[a-zA-Z0-9\-]+\.[a-zA-Z]{2,6}$/g); // /^.+@.+\..+$/g
+let regexZip = new RegExp(/^\d{5}$/g);
 
 // Création d'un objet qui récupère les champs du formulaire
 let contact = {
@@ -191,8 +23,12 @@ const idAddress = "address";
 const idCity = "city";
 const idZip = "zip";
 
-// Récupération du message sous l'input
-let small = document.getElementById("small");
+// Fonction de message sous l'input
+function displayMessage(inputElement, message) {
+    let errorElement = inputElement.nextElementSibling;
+    errorElement.classList.add('active');
+    errorElement.textContent = message;
+}
 
 // Fonction de vérification des inputs
 function checkInputs(idInput, regex) {
@@ -200,12 +36,39 @@ function checkInputs(idInput, regex) {
     let valueInput = input.value;
     if(valueInput.match(regex)) {
         contact[idInput] = valueInput;
-        small.innerHTML = "Ce champ est valide"
+        input.style.border = "solid 3px #009900";
+        displayMessage(input, "Ce champ est correct");
         return true;
     } else {
-        small.innerHTML = "Ce champ est incomplet ou invalide"
+        input.style.border = "solid 3px #FF3300";
+        displayMessage(input, "Ce champ est vide ou incorrect");
         return false;
     }
+}
+
+// Fonction d'envoi du formulaire
+function sendForm() {
+    fetch("http://localhost:3000/api/teddies/order", {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({contact: contact, products: productInStorage})
+    })
+    .then(function(response) {
+        if(response.ok) {
+            return response.json();
+        }
+    })
+    .then(function(value) {
+        let validOrder = JSON.stringify(value);
+        localStorage.setItem("order", validOrder);
+        document.getElementById("form").submit();
+    })
+    .catch(function(err) {
+        window.alert("La page semble rencontrer une erreur, contactez-nous si le problème persiste.")
+    });
 }
 
 // Fonction d'écoute des inputs
@@ -229,7 +92,7 @@ function listenInputs() {
         checkInputs(idZip, regexZip);
     })
 
-    document.getElementById("btnValidOrder");addEventListener("click", (e) => {
+    document.getElementById("form").addEventListener("click", (e) => {
         e.preventDefault();
         if(checkInputs(idFirstName, regexTextOnly) &&
         checkInputs(idLastName, regexTextOnly) &&
@@ -237,9 +100,7 @@ function listenInputs() {
         checkInputs(idAddress, regexText) &&
         checkInputs(idCity, regexTextOnly) &&
         checkInputs(idZip, regexZip)) {
-            return true;
-        }else {
-            return false;
+            sendForm();
         }
     })
 }
